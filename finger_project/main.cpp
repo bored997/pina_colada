b464f3618a1443fa16fa64086082ba564bedf292
Leon Cheung <funarchitect2022@gmail.com>
2016-03-24 22:34:58 -0700

    testing ranges

--- finger_project/main.cpp
+++ finger_project/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    const std::string filename = "imgs/static1.png";
+    const std::string filename = "imgs/static2.png";
     cv::Mat image{ cv::imread(filename) };
 
     if (!image.data)
@@ -14,20 +14,26 @@ int main()
         return -1;
     }
 
-    cv::Mat proc;
-    cv::cvtColor(image, proc, cv::COLOR_BGR2GRAY); // convert to gray
+    cv::Mat hsv;
+    cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV); // convert to hsv
 
+    cv::Mat lowerSkinRange;
+    cv::Mat upperSkinRange;
+    cv::inRange(hsv, cv::Scalar(15, 53, 100), cv::Scalar(20, 255, 255), lowerSkinRange);
+    cv::inRange(hsv, cv::Scalar(25, 100, 100), cv::Scalar(30, 255, 255), upperSkinRange);
+
+    cv::Mat out;
+
+    cv::addWeighted(lowerSkinRange, 1.0, upperSkinRange, 1.0, 0.0, out);
     /*
     Naive method. Doesn't work too well since fingers are multicolored.
     cv::GaussianBlur(proc, proc, cv::Size(25, 25), 0);
     cv::threshold(proc, proc, 160, 255, cv::ThresholdTypes::THRESH_BINARY);
     */
-
-    cv::Canny(proc, proc, 100, 130);
     
     const std::string imageWindow{ "openCV window" };
     cv::namedWindow(imageWindow, cv::WINDOW_AUTOSIZE); // Create a window for display.
-    cv::imshow(imageWindow, proc); // Show our image inside it.
+    cv::imshow(imageWindow, out); // Show our image inside it.
 
     cv::waitKey(0); // Wait for a keystroke in the window
     return 0;
